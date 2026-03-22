ea7d3743b5d8da545c5aae62e99daf3b26bdee7d
Ayush Salaria <149512147+ashmoneykash@users.noreply.github.com>
2026-03-22 17:36:48 +0530

    changed database settings

--- shop/settings.py
+++ shop/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(
         os.environ.get("DATABASE_URL"),
-        conn_max_age=0,
+        conn_max_age=600,
         ssl_require=True
     )
 }
